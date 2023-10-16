a2799d1b1f2bf23d1e796506ca9359e4ad787536
Momen-Daoud7 <momendaoud07@gmail.com>
2023-10-16 05:35:12 +0200

    Build create store api and connect it to UI

---
+++ app/api/stores/route.ts
@@ -0,0 +1,29 @@
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+import prismadb from "@/lib/prismadb";
+
+export async function POST(req: Request) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+    const { name } = body;
+
+    // Check if the user exists
+    if (!userId) {
+      return new NextResponse("Unauthoraized", { status: 401 });
+    }
+
+    // Check if the name is entered
+    if (!name) {
+      return new NextResponse("Name is required", { status: 400 });
+    }
+
+    const store = await prismadb.store.create({ data: { name, userId } });
+
+    return new NextResponse("Store is created.", { status: 200 });
+  } catch (error) {
+    console.log(`[STORE_POST] ${error}`);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
