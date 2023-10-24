a90b8e28e70fbe1a15c7e78fcc0ec4ae08dc5306
Momen-Daoud7 <momendaoud07@gmail.com>
2023-10-24 21:01:54 +0200

    Add stripe payment

---
+++ app/(dashboard)/[storeId]/(routes)/products/[productId]/page.tsx
@@ -0,0 +1,57 @@
+import prismadb from "@/lib/prismadb";
+
+import { ProductForm } from "./components/product-form";
+
+const ProductPage = async ({
+  params
+}: {
+  params: { productId: string, storeId: string }
+}) => {
+
+  let product;
+  try {
+      product = await prismadb.product.findUnique({
+        where: {
+          id: params.productId,
+        },
+        include: {
+          images: true,
+        }
+      });
+  }catch(error) {
+    console.log(error)
+  }
+
+  const categories = await prismadb.category.findMany({
+    where: {
+      storeId: params.storeId,
+    },
+  });
+
+  const sizes = await prismadb.size.findMany({
+    where: {
+      storeId: params.storeId,
+    },
+  });
+
+  const colors = await prismadb.color.findMany({
+    where: {
+      storeId: params.storeId,
+    },
+  });
+
+  return ( 
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <ProductForm 
+          categories={categories} 
+          colors={colors}
+          sizes={sizes}
+          initialData={product || null}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default ProductPage;
