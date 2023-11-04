adb6a48bfd1173c4d63be009e3b009850a1d6bba
Momen-Daoud7 <momendaoud07@gmail.com>
2023-11-04 08:13:40 +0200

    Edit UI

--- app/(dashboard)/[storeId]/(routes)/categories/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/categories/components/client.tsx
@@ -11,24 +11,28 @@ import { ApiAlert } from "@/components/ui/api-alert";
 
 import { columns, CategoryColumn } from "./columns";
 import { ApiList } from "@/components/ui/api-list";
+import Link from "next/link";
 
 interface CategoriesClientProps {
   data: CategoryColumn[];
 }
 
-export const CategoriesClient: React.FC<CategoriesClientProps> = ({
-  data
-}) => {
+export const CategoriesClient: React.FC<CategoriesClientProps> = ({ data }) => {
   const params = useParams();
   const router = useRouter();
 
   return (
     <>
       <div className="flex items-center justify-between">
-        <Heading title={`Categories (${data.length})`} description="Manage categories for your store" />
-        <Button onClick={() => router.push(`/${params.storeId}/categories/new`)}>
-          <Plus className="mr-2 h-4 w-4" /> Add New
-        </Button>
+        <Heading
+          title={`Categories (${data.length})`}
+          description="Manage categories for your store"
+        />
+        <Link href={`/${params.storeId}/categories/new`}>
+          <Button>
+            <Plus className="mr-2 h-4 w-4" /> Add New
+          </Button>
+        </Link>
       </div>
       <Separator />
       <DataTable searchKey="name" columns={columns} data={data} />
